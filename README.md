0e1ad0e5ede42667f89369a9e9e509c22204ce41
jakeshivers <jakeshivers@gmail.com>
2024-09-26 20:00:29 -0600

    adding new files and working on APIs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DEC-project-1
-Project one for Data Engineer Camp
+Project one for Data Engineer Camp. We need to add more info here once we get going.
--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,15 @@
-import grids
+import pycurl
+from io import BytesIO
 
-gridstatus.__version__
+buffer = BytesIO()
+c = pycurl.Curl()
+#c.setopt(c.URL, 'https://httpbin.org/get')
+#c.setopt(c.URL, 'https://httpbin.org/get?dog=dog')
+c.setopt(c.URL, 'https://oasis.caiso.com/mrtu/GroupZip?groupid=RTM_LMP_GRP&startdate=20240901&opr_hr=01')
 
-iso = gridstatus.CAISO()
-iso.list_oasis_datasets()
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+print(body.decode('utf-8'))
