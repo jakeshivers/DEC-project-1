903bbfa8f58f57ca242fc6af293f96b1ec272bec
jakeshivers <jakeshivers@gmail.com>
2024-09-28 14:23:16 -0600

    made a connection to localhost, filled table with data

---
+++ etl_project/assets/petroleum.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+from etl_project.connectors.postgresql import PostgreSqlClient
+from etl_project.connectors.petroleum import EiaApiClient
+from sqlalchemy import Table, MetaData
+
+
+def extract(
+    eai_api_client: EiaApiClient
+):
+    response_data = []
+    
+    #todo what is meta?    
+    df = pd.json_normalize(data=response_data, meta=["something?"])
+    return df
+   
+
+
+
+def transform(df: pd.DataFrame, df_petroleum: pd.DataFrame) -> pd.DataFrame:
+    # rename columns to more meaningful names
+    df_quotes_renamed = df.rename(
+        columns={
+
+        }
+    )
+
+
+def load(
+    df_petroleum: pd.DataFrame, postgresql_client: PostgreSqlClient, table, metadata
+):
+    print(df_petroleum)
+    exit
+    postgresql_client.write_to_table(
+        data=df_petroleum.to_dict(orient="records"), table=table, metadata=metadata
+    )
